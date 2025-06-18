adac3a6ab8d92bae1c3b1f0854a568671f653dfb
seongho00 <tjdwns000927@gmail.com>
2025-06-18 17:43:20 +0900

    DB 업데이트

--- src/main/java/com/example/demo/controller/UsrPlannerController.java
+++ src/main/java/com/example/demo/controller/UsrPlannerController.java
@@ -144,8 +144,6 @@ public class UsrPlannerController {
 
 		// 오늘의 일정들
 		List<TripPlace> todayTripPlaces = plannerService.getTripPlace(tripDays);
-		
-		System.out.println(todayTripPlaces);
 
 		model.addAttribute("tripInfo", tripInfo);
 		model.addAttribute("todayTripPlaces", todayTripPlaces);
